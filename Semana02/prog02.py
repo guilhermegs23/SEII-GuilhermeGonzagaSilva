03bea63b88a68f251c71cf9f3e9f08b5eb57384b
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-13 20:51:34 -0300

    commit by Guilherme

---
+++ Semana02/prog02.py
@@ -0,0 +1,82 @@
+#Guilherme Gonzaga Silva 11621EMT021
+
+#1
+#print('Hello world')
+
+#2
+#message = 'Hello World'
+#print(message)
+
+#3
+#message = 'Bobby\'s World'
+#print(message)
+
+#4
+#message = "Bobby's World"
+#print(message)
+
+#5
+#message = """Bobby's World was a good
+#cartoon in the 1990s"""
+#print(message)
+
+#6
+#message = 'Hello World'
+#print(len(message))
+
+#7
+#message = 'Hello World'
+#print(message[10])
+
+#8
+#message = 'Hello World'
+#print(message[:5])
+
+#9
+#message = 'Hello World'
+#print(message.lower())
+
+#10
+#message = 'Hello World'
+#print(message.upper())
+
+#11
+#message = 'Hello World'
+#print(message.count(Hello))
+
+#12
+#message = 'Hello World'
+#print(message.find(Hello))
+
+#13
+#message = 'Hello World'
+#new_message = message.replace('World','Universe')
+#print(new_message)
+
+#14
+#greeting = 'Hello'
+#name = 'Michael'
+#message = greeting + ', ' + name + '. Welcome!'
+#print(message)
+
+#15
+#greeting = 'Hello'
+#name = 'Michael'
+#message = '{}, {}. Welcome!'.format(greeting,name)
+#print(message)
+
+#16
+#greeting = 'Hello'
+#name = 'Michael'
+#message = f'{greeting}, {name.upper()}. Welcome!'
+#print(message)
+
+#17
+#greeting = 'Hello'
+#name = 'Michael'
+#print(dir(name))
+
+#17
+#greeting = 'Hello'
+#name = 'Michael'
+#print(help(str.lower))
