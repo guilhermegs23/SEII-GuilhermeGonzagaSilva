261492903d6d09bd48f9b75002c2ecc609018fa2
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-14 13:17:14 -0300

    commit by Guilherme

---
+++ Semana02/prog07.py
@@ -0,0 +1,91 @@
+#Guilherme Gonzaga Silva 11621EMT021 
+#prog07.py
+
+# nums = [1, 2, 3, 4, 5]
+# for num in nums:
+#     if num == 3:
+#         print('Found')
+#         break
+#     print(num) #resultados
+# # 1
+# # 2
+# # Found
+
+# nums = [1, 2, 3, 4, 5]
+# for num in nums:
+#     if num == 3:
+#         print('Found')
+#         continue
+#     print(num) #resultados
+# # 1
+# # 2
+# # Found
+# # 4
+# # 5
+
+# nums = [1, 2, 3, 4, 5]
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter) #resultados
+# # 1 a
+# # 1 b
+# # 1 c
+# # 2 a
+# # 2 b
+# # 2 c
+# # 3 a
+# # 3 b
+# # 3 c
+# # 4 a
+# # 4 b
+# # 4 c
+# # 5 a
+# # 5 b
+# # 5 c
+
+# for i in range(1, 11):
+#     print(i)
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+# # 6
+# # 7
+# # 8
+# # 9
+# # 10
+
+# x = 0
+# while x < 10:
+#     print(x)
+#     x+=1 #resultados
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+# # 6
+# # 7
+# # 8
+# # 9
+
+# x = 0
+# while x < 10:
+#     if x == 5: 
+#         break
+#     print(x)
+#     x+=1 #resultados
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
+
+x = 0
+while True:
+    # if x == 5: 
+    #     break
+    print(x)
+    x+=1 
