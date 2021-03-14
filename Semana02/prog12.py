413caf3e61f02b513733e8a6f9cd85e6bcd43b7c
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-14 17:28:34 -0300

    commit by Guilherme

---
+++ Semana02/prog12.py
@@ -0,0 +1,13 @@
+#Guilherme Gonzaga Silva 11621EMT021 
+#prog12.py
+
+'''Não ouve código durante a vídeo aula'''
+
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+'''3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)]
+C:\Users\guilh\AppData\Local\Programs\Python\Python38-32\python.exe
+'''
