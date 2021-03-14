b1103bc8f917ac3bfff2f47ab7d368075c95a762
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-14 15:21:31 -0300

    commit by Guilherme

---
+++ Semana02/prog10.py
@@ -0,0 +1,33 @@
+#Guilherme Gonzaga Silva 11621EMT021 
+#prog10.py
+
+import os
+from datetime import datetime
+
+os.chdir("C:")
+# print(dir(os))
+# print(os.getcwd())
+# os.makedir("diretoriocriado/Sub-Dir-1")
+# os.makedirs("diretoriocriado/Sub-Dir-1")
+# os.removedirs("diretoriocriado/Sub-Dir-1")
+# os.rename('READ.ME', 'LEIA.ME')
+# print(os.stat('LEIA.ME').st_size)
+# mod_time = os.stat('LEIA.ME').st_mtime
+# print(datetime.fromtimestamp(mod_time)) #2021-03-14 15:02:30.106765
+# print(os.listdir())
+
+# for dirpath, dirnames, filenames in os.walk('C:'):
+#     print('Current Path: ', dirpath)
+#     print('Directories: ', dirnames)
+#     print('Files: ', filenames)
+#     print()
+
+# os.environ.get('prog09')
+# file_path = os.path.join(os.environ.get('Teste'), 'test.txt')
+# print(file_path)
+
+# print(os.path.basename('prog02.py'))
+# print(os.path.dir('prog02.py'))
+# print(os.path.split('prog02.py'))
+# print(os.path.exits('prog02.py'))
+# print(os.path.splitext('prog02.py'))
