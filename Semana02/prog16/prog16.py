0bfad9972831303e8b94693e7e7af3566daf6bf4
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-15 12:57:34 -0300

    commit by Guilherme

--- Semana02/prog16/prog16.py
+++ Semana02/prog16/prog16.py
@@ -6,7 +6,7 @@ import csv
 html_output = ''
 names = []
 
-with open('patrons.csv', 'r') as data_file:
+with open('patreon.csv', 'r') as data_file:
     #csv_data = csv.reader(data_file)
     csv_data = csv.DictReader(data_file)
 
