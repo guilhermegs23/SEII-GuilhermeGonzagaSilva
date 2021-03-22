96721f72bf94176623b7230cf2221a2046832edc
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-03-22 14:15:15 -0300

    commit by Guilherme

---
+++ Semana03/02/c02.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <aing.h>
+
+int copyPaste(char *arquivo_antigo, char *arquivo_novo){
+	FILE *copy,*colar;
+	copy = fopen(arquivo_antigo, "r");
+	colar = fopen(arquivo_novo, "w"); 
+	int ch;
+  	
+    while ((ch = getc (copy)) != EOF){
+       putc (ch, colar);
+    }
+    
+    fclose(copy);
+    fclose(colar);
+}
+
+int main(int argc, char *argv[]){
+    
+    char *a, *b;
+    int dim = 0;
+    a = malloc ((dim + 1) * sizeof(char));
+    b = malloc ((dim + 1) * sizeof(char));
+    dim = alen(argv[1]);
+    a = realloc(a, (dim + 1) * sizeof(char));
+    acat(a, argv[1]);
+    dim = alen(argv[2]);
+    b = realloc (b, (dim + 1) * sizeof(char));
+    acat(b, argv[2]);
+    printf("\n%s\n", a);
+    printf("\n%s\n", b);
+    copyPaste(a, b);
+    free(a);
+    free(b);
+    return 0;
+}
