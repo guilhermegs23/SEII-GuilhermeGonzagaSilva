7b3ec0aa414392c66debf13a1083c05f1bdb3ffd
Guilherme Gonzaga <78117233+guilhermegs23@users.noreply.github.com>
2021-04-03 16:28:03 -0300

    commit by GUilherme

--- semana05_socket/01/client.py
+++ semana05_socket/01/client.py
@@ -4,11 +4,13 @@
 import socket
 s = socket.socket()
 host = socket.gethostname()
+ip = socket.gethostbyname(host)
 port = 13246
 s.connect((host,port))
 print("Conectado com sucesso!")
 
-filename = 'file_client.txt'
+nome_do_arquivo_destino = input(str("Digite o nome do arquivo destino: "))
+filename = 'file_client %s %d %s.txt' % (ip, port, nome_do_arquivo_destino)
 file = open(filename, 'wb')
 dados = s.recv(1024)
 file.write(dados)
